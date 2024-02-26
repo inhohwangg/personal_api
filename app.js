12a368cdf18083aff2c7e52966d23f185bdfa6fb
hwanginho <hwanginho@gractor.com>
2024-02-26 14:54:23 +0900

    db remove

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
-const db = require('./dbConnection');
 const userRouters = require('./controller/users');
 
 app.use(bodyParser.urlencoded({ extended: true })); // 'urlcoded'를 'urlencoded'로 수정
