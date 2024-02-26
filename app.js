e81077a3799ea55764d9cca2f481ed9a75956ddd
inhohwangg <sk1440sk@naver.com>
2024-02-27 00:22:00 +0900

    미들웨어를 사용해서 모든 api 요청시 setHeaders 적용

--- app.js
+++ app.js
@@ -7,9 +7,17 @@ const userRouters = require('./controller/users');
 app.use(bodyParser.urlencoded({ extended: true })); // 'urlcoded'를 'urlencoded'로 수정
 app.use(bodyParser.json());
 app.use('/users', userRouters);
+app.use((req, res, next) => {
+    res.setHeader('x-inho-api', '1.0.0');
+    next();
+})
 
 app.get('/', (req, res) => {
     res.json({ user: 'http://api.example.com/users?page=2' });
+});
+
+app.get('/test', (req, res) => {
+    res.json({ user: 'http://api.example.com/users?page=3' });
 });
 
 app.post('/', (req, res) => {
@@ -25,5 +33,5 @@ app.delete('/user', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
