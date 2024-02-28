21cd9ae061366e272be0eedf7e83a55f189c92b9
inhohwangg <sk1440sk@naver.com>
2024-02-29 01:06:04 +0900

    header 에 token 값 입력할때 앞에 Bearer 안 붙이게 설정

--- controller/auth-middleware.js
+++ controller/auth-middleware.js
@@ -2,11 +2,11 @@ const jwt = require('jsonwebtoken')
 
 const authticateToken = (req, res, next) => {
 	const authHeader = req.headers['x-viewtrack-token'];
-	console.log(authHeader.split(' ')[1]);
-	const token = authHeader && authHeader.split(' ')[1];
-	if (token == null) return res.sendStatus(401);
+	// console.log(authHeader);
+	// const token = authHeader && authHeader.split(' ')[1];
+	if (!authHeader) return res.sendStatus(401);
 
-	jwt.verify(token, 'secretKey', (err, user) => {
+	jwt.verify(authHeader, 'secretKey', (err, user) => {
 		if (err) return res.sendStatus(403);
 		req.user = user;
 		next();
