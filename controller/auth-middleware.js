4542b92423358828d41af7c429558a1fed904a4e
inhohwangg <sk1440sk@naver.com>
2024-02-29 01:13:28 +0900

    Bearer 붙여도 조회가능 , 안붙여도 조회가능하게 기능 수정

--- controller/auth-middleware.js
+++ controller/auth-middleware.js
@@ -2,11 +2,21 @@ const jwt = require('jsonwebtoken')
 
 const authticateToken = (req, res, next) => {
 	const authHeader = req.headers['x-viewtrack-token'];
+	let token;
+
+	if (authHeader) {
+		if (authHeader.startsWith('Bearer ')) {
+			token = authHeader.substring(7, authHeader.length);
+		} else {
+			token = authHeader;
+		}
+	}
+
 	// console.log(authHeader);
 	// const token = authHeader && authHeader.split(' ')[1];
 	if (!authHeader) return res.sendStatus(401);
 
-	jwt.verify(authHeader, 'secretKey', (err, user) => {
+	jwt.verify(token, 'secretKey', (err, user) => {
 		if (err) return res.sendStatus(403);
 		req.user = user;
 		next();
