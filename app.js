2e292405a7fdf847c3e219248da7f0b57c501a44
inhohwangg <sk1440sk@naver.com>
2024-03-14 00:19:03 +0900

    todoRouters 추가

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const userRouters = require('./controller/users');
+const todoRouters = require('./controller/todo')
 
 app.use(bodyParser.urlencoded({ extended: true })); // 'urlcoded'를 'urlencoded'로 수정
 app.use(bodyParser.json());
-app.use('/users', userRouters);
+app.use('/api/users', userRouters);
+app.use('/api/todo', todoRouters);
 app.use((req, res, next) => {
     res.setHeader('x-inho-api', '1.0.0');
     next();
