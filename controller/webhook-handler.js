774e4a606bc910a3f8b04d01b37054f68fe64ff4
hwanginho <sh1440sk@naver.com>
2024-07-10 13:54:07 +0900

    .

--- controller/webhook-handler.js
+++ controller/webhook-handler.js
@@ -18,8 +18,8 @@ router.post('/payload', (req, res) => {
     const payload = req.body;
 
     if (payload.ref === 'refs/heads/main') { // 원하는 브랜치인지 확인
-        const cmd = 'git config --global --add safe.directory /home/hwanginho/dev/personal_api && cd /home/hwanginho/dev/personal_api && sudo git pull origin main && sudo systemctl restart personal-api.service';
-        exec(cmd, (error, stdout, stderr) => {
+        const cmd = 'cd /home/hwanginho/dev/personal_api && git pull && sudo systemctl restart personal-api.service';
+        exec(cmd, { env: { ...process.env, HOME: '/home/hwanginho' } }, (error, stdout, stderr) => {
             if (error) {
                 console.error(`exec error: ${error}`);
                 res.status(500).send(`exec error: ${error}`);
