918ff9c6a996fd929d6712affd82f1714dadd175
inhohwangg <sk1440sk@naver.com>
2024-07-30 00:06:20 +0900

    fields 값 동적으로 변경

--- controller/todo.js
+++ controller/todo.js
@@ -18,7 +18,8 @@ const storage = multer.diskStorage({
 	},
 	filename: function (req, file, cb) {
 		const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-		cb(null, uniqueSuffix + path.extname(file.originalname));
+		const extension = path.extname(file.originalname);
+		cb(null, uniqueSuffix + extension);
 	}
 })
 const upload = multer({ storage: storage });
